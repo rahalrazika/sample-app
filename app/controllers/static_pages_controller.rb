d6731178875698309cc057b0ce66591e592909a4
rahalrazika <rahalraika@gmail.com>
2020-10-24 19:49:03 +0200

    add the sass style and make a the links work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,5 @@ class StaticPagesController < ApplicationController
   def help; end
 
   def about; end
+  def contact; end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-
-
   root 'static_pages#home'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get 'signup', to: 'users#new'
+  end
+  
